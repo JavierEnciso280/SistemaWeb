601876735821da61c0ac9ac05d0f78c41f701552
Javier <encisojavier280@gmail.com>
2022-11-14 13:35:14 -0300

    hasta aca el sidebar hasta referenciales generales

--- main.php
+++ main.php
@@ -24,7 +24,7 @@
     <title>Login</title>
     </head>
     <body class="skin-blue fixed">
-        <div class="wrapper">
+        <div class="wrapper"><!--contenedor del logo y el aside-->
             <header class="main-header">
                 <a href="#" class="logo">
                     <img src="assets/img/log.png" alt="Logo SysWeb">
@@ -40,10 +40,24 @@
                     </div>
                 </nav>
             </header>
+            <aside class="main-sidebar"><!--menu lateral del main-->
+                <section class="sidebar">
+                    <?php include 'sidebar-menu.php';?><!--proceso en sidebar-menu.php-->
+                </section>
+            </aside>
         </div>
 
 
     <script src="assets/plugins/jQuery/jQuery-2.1.3.min.js"></script>
     <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/plugins/datepicker/bootstrap-datepicker.min.js"></script>
+    <script src="assets/plugins/chosen/js/chosen.jquery.min.js"></script>
+    <script src="assets/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
+    <script src="assets/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
+    <script src="assets/plugins/slimScroll/jquery.slimscroll.js" type="text/javascript"></script>
+    <script src="assets/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <script src="assets/plugins/fastclick/fastclick.min.js" type="text/javascript"></script>
+    <script src="assets/js/jquery.maskMoney.min.js" type="text/javascript"></script>
+    <script src="assets/js/app.js" type="text/javascript"></script>
     </body>
 </html>
---
+++ sidebar-menu.php
@@ -0,0 +1,36 @@
+<?php
+//la funcion session start() del main ya trae todos los parametros del login-check.php
+
+if($_SESSION['permisos_acceso']=='Super Admin'){
+?>
+<!--etiquetas html aqui-->
+<ul class="sidebar-menu">
+    <li class="header">Menu</li>
+    <?php
+    if($_GET["module"]="start"){
+        $active_home="active";
+    }else{
+        $active_home ="";
+    }?>
+    
+    <li class="<?php echo $active_home;?>">
+        <a href="?module=start"><i class="fa fa-home">Inicio</li></a>
+    </li>
+    <?php 
+    if($_GET["module"]=="start"){?>
+        <li class="treeview">
+            <a href="javascript:void(0);">
+                <i class="fa fa-file-text"></i><span>Referenciales Generales</span><i class="fa fa-angle-left pull-right"></i>
+            </a>
+            <ul class="treeview-menu">
+                <li><a href="#" ><i class="fa fa-circle-o"></i>Ciudad</a></li>
+            </ul>
+        </li>
+    <?php
+    }
+    ?>
+
+</ul>
+<?php
+}
+?>
